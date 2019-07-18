9b3b3a08454d02d9274977705e920316f261b7b9
Mobius1729 <31332999+Mobius1729@users.noreply.github.com>
2019-07-18 14:56:46 -0400

    Update reddit_webScraper.py

--- reddit_webScraper.py
+++ reddit_webScraper.py
@@ -5,25 +5,34 @@
 
 
 import praw
-import pandas as pd
 import datetime as dt
 import json
 import csv
-from time import sleep
 
-reddit = praw.Reddit(client_id='4hqdHPpJDAGsJg',                      client_secret='O8Qirn8FgGUNi55Mh_Us4_Mqtls',                      user_agent='ProgrammerHumor Web Scraper',                      username='Dankious_memeious1',                      password='@Liverpool123')
+#initalize praw
+reddit = praw.Reddit(client_id='4hqdHPpJDAGsJg',                      
+                     client_secret='O8Qirn8FgGUNi55Mh_Us4_Mqtls',                      
+                     user_agent='ProgrammerHumor Web Scraper',                      
+                     username='***********',                      
+                     password='***********')
 
+#get subreddit (r/DailyProgrammer)
 subreddit = reddit.subreddit('DailyProgrammer')
 
+#sort the subreddit by hot
 sort_hot_subreddit = subreddit.hot(limit = (26))
 
-def getDate(created):
-    return dt.datetime.fromtimestamp(created)
+#all the elemnts of the post we wish to get
+topics_dict = {"title": [],               
+               "score" : [],               
+               "id" : [],               
+               "numOfComments" : [],               
+               "created": [],               
+               "body" : [],               
+               "upvotes" : [],
+              "flair" : []}
 
-
-
-topics_dict = {"title": [],               "score" : [],               "id" : [],               "numOfComments" : [],               "created": [],               "body" : [],               "upvotes" : [],               "downvotes" : []}
-
+#append elements from post to apropriate lists.
 for submission in sort_hot_subreddit:
     topics_dict["title"].append(submission.title)
     topics_dict["score"].append(submission.score)
@@ -31,24 +40,16 @@ for submission in sort_hot_subreddit:
     topics_dict["numOfComments"].append(submission.num_comments)
     topics_dict["created"].append(submission.created)
     topics_dict["body"].append(submission.selftext)
+    topics_dict["upvotes"].append(submission.upvote_ratio)
+    topics_dict["flair"].append(submission.link_flair_text)
 
 
-data_to_csv = topics_data.to_csv('FILENAME.csv', index=False) 
-json_str = json.dumps(topics_dict).encode('ascii')
-json_str_prettyPrint = json.dumps(topics_dict, sort_keys = True, indent = 2, separators=("yeet", "New challenge"))
+data_to_csv = topics_data.to_csv('FILENAME.csv', index = False) #convert data to a CSV file
+json_str = json.dumps(topics_dict).encode('utf-8') #convert data to a JSON file
+json_str_prettyPrint = json.dumps(topics_dict, sort_keys = True, indent = 2, separators=("\n", ";")) #set JSON to pretty printing
 
+#write contents to JSON file
 with open('json_str.json', 'w') as outfile:
     json.dump(topics_dict, outfile)
 
-
-
-    
-
 print(json_str_prettyPrint)
-
-
-# In[ ]:
-
-
-
-
